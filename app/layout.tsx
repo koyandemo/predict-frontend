bd41d0d738a6bc84048b8856001aa8eb317fa312
Wai Yan <koyannote2180@gmail.com>
2026-04-13 23:50:28 +0700

    feat: add dark className in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <link rel="icon" href="/logo.png" />
       <body className={`${inter.className} font-sans antialiased`}>
         <AuthProviderClient>
