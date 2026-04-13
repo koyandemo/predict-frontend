42a0c1f8e62c15e7eab96b50720ce17babb66283
Wai Yan <koyannote2180@gmail.com>
2026-04-13 23:09:54 +0700

    feat: update matches list

--- app/(web)/matches/page.tsx
+++ app/(web)/matches/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { MatchesList } from "./_components/MatchList";
 
 export default function MatchesPage() {
   return (
     <div className="container min-h-screen mx-auto px-4 py-6 md:py-8">
+      <Suspense fallback={<div>Loading...</div>}>
       <MatchesList />
+      </Suspense>
     </div>
   );
 }
